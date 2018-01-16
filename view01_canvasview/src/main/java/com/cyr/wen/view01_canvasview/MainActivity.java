0f98ad8fad50edd1557f1f0b3b6e092ec15ee758
czw <985051537@qq.com>
2018-01-16 21:28:28 +0800

    第一次提交

--- view01_canvasview/src/main/java/com/cyr/wen/view01_canvasview/MainActivity.java
+++ view01_canvasview/src/main/java/com/cyr/wen/view01_canvasview/MainActivity.java
@@ -3,6 +3,7 @@ package com.cyr.wen.view01_canvasview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//测试一下
 public class MainActivity extends AppCompatActivity {
 
     @Override
